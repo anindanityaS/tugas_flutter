58c91c65ce08ce436400fa87a2f4d1a45d16f289
anindanityaS <anindanitya22@gmail.com>
2020-10-29 21:16:02 +0700

    edit file

--- lib/home.dart
+++ lib/home.dart
@@ -19,62 +19,145 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Container(
-              width: 300,
-              height: 60,
-              color: Colors.red,
-              alignment: Alignment.center,
-              margin: EdgeInsets.all(16),
-              child: Text(
-                "Indonesia",
-                style: TextStyle(color: Colors.white, fontSize: 24),
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: MediaQuery.of(context).size.width / 4,
+                  height: 60,
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.all(16),
+                  child: Image.asset('lib/image/Indonesia.png'),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width / 2,
+                  height: 100,
+                  color: Colors.red,
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.all(16),
+                  child: Text(
+                    "Indonesia",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                ),
+              ],
             ),
-            Container(
-              width: 300,
-              height: 60,
-              color: Colors.red,
-              alignment: Alignment.center,
-              margin: EdgeInsets.all(16),
-              child: Text(
-                "Malaysia",
-                style: TextStyle(color: Colors.white, fontSize: 24),
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: MediaQuery.of(context).size.width / 4,
+                  height: 60,
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.all(16),
+                  child: Image.asset('lib/image/Malaysia.png'),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width / 2,
+                  height: 100,
+                  color: Colors.blue,
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.all(16),
+                  child: Text(
+                    "Malaysia",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                ),
+              ],
             ),
-            Container(
-              width: 300,
-              height: 60,
-              color: Colors.red,
-              alignment: Alignment.center,
-              margin: EdgeInsets.all(16),
-              child: Text(
-                "Singapura",
-                style: TextStyle(color: Colors.white, fontSize: 24),
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: MediaQuery.of(context).size.width / 4,
+                  height: 60,
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.all(16),
+                  child: Image.asset('lib/image/Singapore.png'),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width / 2,
+                  height: 60,
+                  color: Colors.red,
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.all(16),
+                  child: Text(
+                    "Singapura",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                ),
+              ],
             ),
-            Container(
-              width: 300,
-              height: 60,
-              color: Colors.red,
-              alignment: Alignment.center,
-              margin: EdgeInsets.all(16),
-              child: Text(
-                "Filipina",
-                style: TextStyle(color: Colors.white, fontSize: 24),
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: MediaQuery.of(context).size.width / 4,
+                  height: 60,
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.all(16),
+                  child: Image.asset('lib/image/Kamboja.png'),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width / 2,
+                  height: 60,
+                  color: Colors.blue,
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.all(16),
+                  child: Text(
+                    "Kamboja",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                ),
+              ],
             ),
-            Container(
-              width: 300,
-              height: 60,
-              color: Colors.red,
-              alignment: Alignment.center,
-              margin: EdgeInsets.all(16),
-              child: Text(
-                "Thailand",
-                style: TextStyle(color: Colors.white, fontSize: 24),
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: MediaQuery.of(context).size.width / 4,
+                  height: 60,
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.all(16),
+                  child: Image.asset('lib/image/Thailand.png'),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width / 2,
+                  height: 60,
+                  color: Colors.red,
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.all(16),
+                  child: Text(
+                    "Thailand",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: MediaQuery.of(context).size.width / 4,
+                  height: 60,
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.all(16),
+                  child: Image.asset('lib/image/Brunei.png'),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width / 2,
+                  height: 60,
+                  color: Colors.red,
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.all(16),
+                  child: Text(
+                    "Brunei",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
